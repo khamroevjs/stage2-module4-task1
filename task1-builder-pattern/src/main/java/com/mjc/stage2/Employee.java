84525efd1d7c337ccf6e1fec3ca7c3497cf621c7
Khamroev <khamroevjs@gmail.com>
2023-07-12 16:06:39 +0500

    task done

--- task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
+++ task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
@@ -1,5 +1,6 @@
 package com.mjc.stage2;
 
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
@@ -69,6 +70,44 @@ public class Employee {
     }
 
     public static class EmployeeBuilder {
-        // Write your code here!
+        private final Employee employee;
+
+        public EmployeeBuilder() {
+            employee = new Employee();
+        }
+
+        public EmployeeBuilder setName(String name) {
+            employee.name = name;
+            return this;
+        }
+
+        public EmployeeBuilder setLastName(String lastName) {
+            employee.lastName = lastName;
+            return this;
+        }
+
+        public EmployeeBuilder setPosition(String position) {
+            employee.position = position;
+            return this;
+        }
+
+        public EmployeeBuilder setPhone(String phone) {
+            employee.phone = phone;
+            return this;
+        }
+
+        public EmployeeBuilder setEmail(String email) {
+            employee.email = email;
+            return this;
+        }
+
+        public EmployeeBuilder setCarNumber(String carNumber) {
+            employee.carNumber = carNumber;
+            return this;
+        }
+
+        public Employee build() {
+            return employee;
+        }
     }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,33 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection() {
+        Properties props = new Properties();
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        try (InputStream stream = loader.getResourceAsStream("h2database.properties")) {
+            props.load(stream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String url = props.getProperty("db_url");
+        String user = props.getProperty("user");
+        String password = props.getProperty("password");
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
 
